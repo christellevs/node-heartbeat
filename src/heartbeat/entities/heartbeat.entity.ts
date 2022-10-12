d620745b0566b69ddff2d9d6b2bccabca73b3e66
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-12 21:33:25 +0100

    feat: create entity for heartbeat

---
+++ src/heartbeat/entities/heartbeat.entity.ts
@@ -0,0 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsInt, IsUrl } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class Heartbeat {
+  @ApiProperty({
+    description: 'The automatically generated ID of the heartbeat in the db',
+    readOnly: true,
+  })
+  @IsInt()
+  readonly id!: number;
+
+  @ApiProperty({
+    description: 'The provided Heartbeat ID',
+    example: 'e335175a-eace-4a74-b99c-c6466b6afadd',
+  })
+  @IsUrl()
+  heartbeatId!: string;
+
+  @ApiProperty({
+    description: 'First registered heartbeat',
+    readOnly: true,
+  })
+  @Type(() => Date)
+  @IsDate()
+  readonly createdAt!: Date;
+
+  @ApiProperty({
+    description: 'Last registered heartbeat',
+    readOnly: true,
+  })
+  @Type(() => Date)
+  @IsDate()
+  readonly updatedAt!: Date;
+
+  @ApiProperty({
+    description: 'The name of the group the heartbeat is part of',
+    example: 'particle-detector',
+  })
+  @IsUrl()
+  group!: string;
+
+  @ApiProperty({
+    description: 'Meta information attached to request body',
+    example: '{ "foo": 1 }',
+  })
+  @IsUrl()
+  meta!: string;
+}
--- src/heartbeat/heartbeat.service.ts
+++ src/heartbeat/heartbeat.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Heartbeat } from './entities/heartbeat.entity';
 
 @Injectable()
 export class HeartbeatService {
   constructor(private prisma: PrismaService) {}
 
-  getHeartbeats(group: string) {
-    return this.prisma.heartbeat.findMany({ where: { group: group } });
+  async getHeartbeats(group: string): Promise<Heartbeat[]> {
+    return await this.prisma.heartbeat.findMany({ where: { group: group } });
   }
 }
