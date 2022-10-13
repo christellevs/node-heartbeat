a1ae3f48555d0270541e5e5f2b43c31fcd850de0
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-13 21:29:08 +0100

    test: unit test for heartbeat controller

--- src/heartbeat/heartbeat.controller.spec.ts
+++ src/heartbeat/heartbeat.controller.spec.ts
@@ -1,11 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HeartbeatController } from './heartbeat.controller';
 import { HeartbeatService } from './heartbeat.service';
+import { Meta } from './entities/meta.entity';
 
 describe('HeartbeatController', () => {
   let controller: HeartbeatController;
+  let dto = { meta: { foo: '1' } };
+  let id = '123-123';
+  let group = 'desperados';
+  let instances = 1;
 
-  const mockHeartbeatService = {};
+  const mockHeartbeatService = {
+    create: jest.fn((group, id, dto) => {
+      return {
+        id,
+        group,
+        ...dto,
+      };
+    }),
+    delete: jest.fn((group, id) => {
+      return {
+        id,
+        group,
+        ...dto,
+      };
+    }),
+    findAllByGroup: jest.fn((group) => {
+      return [
+        {
+          id,
+          group,
+          ...dto,
+        },
+      ];
+    }),
+    getSummary: jest.fn(() => {
+      return [
+        {
+          group,
+          instances,
+        },
+      ];
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,5 +58,46 @@ describe('HeartbeatController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a heartbeat', () => {
+    expect(controller.create(group, id, dto)).toEqual({
+      id: '123-123',
+      group: 'desperados',
+      meta: { foo: '1' },
+    });
+
+    expect(mockHeartbeatService.create).toHaveBeenCalledWith(group, id, dto);
+  });
+
+  it('should get a heartbeat by group', () => {
+    expect(controller.findAllByGroup(group)).toEqual([
+      {
+        id: '123-123',
+        group: 'desperados',
+        meta: { foo: '1' },
+      },
+    ]);
+
+    expect(mockHeartbeatService.findAllByGroup).toHaveBeenCalledWith(group);
+  });
+
+  it('should delete a heartbeat by group and id', () => {
+    expect(controller.delete(group, id)).toEqual({
+      id: '123-123',
+      group: 'desperados',
+      meta: { foo: '1' },
+    });
+
+    expect(mockHeartbeatService.delete).toHaveBeenCalledWith(group, id);
+  });
+
+  it('should return groups summary', () => {
+    expect(controller.summary()).toEqual([
+      {
+        group: 'desperados',
+        instances: 1,
+      },
+    ]);
   });
 });
