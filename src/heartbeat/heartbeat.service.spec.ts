29ce1c50ef2dc6443e5baa479d71dbdce4702b5f
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-13 21:58:09 +0100

    test: add test spec for heartbeat service with promises

--- src/heartbeat/heartbeat.service.spec.ts
+++ src/heartbeat/heartbeat.service.spec.ts
@@ -4,8 +4,44 @@ import { HeartbeatController } from './heartbeat.controller';
 
 describe('HeartbeatService', () => {
   let service: HeartbeatService;
+  let dto = { meta: { foo: '1' } };
+  let id = '123-123';
+  let group = 'desperados';
+  let instances = 1;
 
-  const mockHeartbeatModel = {};
+  const mockHeartbeatService = {
+    create: jest.fn().mockImplementation((group, id, dto) =>
+      Promise.resolve({
+        id,
+        group,
+        ...dto,
+      }),
+    ),
+    delete: jest.fn().mockImplementation((group, id) =>
+      Promise.resolve({
+        id,
+        group,
+        ...dto,
+      }),
+    ),
+    findAllByGroup: jest.fn().mockImplementation((group) =>
+      Promise.resolve([
+        {
+          id,
+          group,
+          ...dto,
+        },
+      ]),
+    ),
+    getSummary: jest.fn().mockImplementation(() =>
+      Promise.resolve([
+        {
+          group,
+          instances,
+        },
+      ]),
+    ),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,7 +49,7 @@ describe('HeartbeatService', () => {
       providers: [HeartbeatService],
     })
       .overrideProvider(HeartbeatService)
-      .useValue(mockHeartbeatModel)
+      .useValue(mockHeartbeatService)
       .compile();
 
     service = module.get<HeartbeatService>(HeartbeatService);
@@ -21,5 +57,46 @@ describe('HeartbeatService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a heartbeat', async () => {
+    expect(await service.create(group, id, dto)).toEqual({
+      id: '123-123',
+      group: 'desperados',
+      meta: { foo: '1' },
+    });
+
+    expect(mockHeartbeatService.create).toHaveBeenCalledWith(group, id, dto);
+  });
+
+  it('should get a heartbeat by group', async () => {
+    expect(await service.findAllByGroup(group)).toEqual([
+      {
+        id: '123-123',
+        group: 'desperados',
+        meta: { foo: '1' },
+      },
+    ]);
+
+    expect(mockHeartbeatService.findAllByGroup).toHaveBeenCalledWith(group);
+  });
+
+  it('should delete a heartbeat by group and id', async () => {
+    expect(await service.delete(group, id)).toEqual({
+      id: '123-123',
+      group: 'desperados',
+      meta: { foo: '1' },
+    });
+
+    expect(mockHeartbeatService.delete).toHaveBeenCalledWith(group, id);
+  });
+
+  it('should return groups summary', async () => {
+    expect(await service.getSummary()).toEqual([
+      {
+        group: 'desperados',
+        instances: 1,
+      },
+    ]);
   });
 });
