c278b725f9388ebc4f5521f6f49ef484be367be6
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-13 20:42:15 +0100

    test: fix initial testing for heartbeat service

--- src/heartbeat/heartbeat.service.spec.ts
+++ src/heartbeat/heartbeat.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HeartbeatService } from './heartbeat.service';
+import { HeartbeatController } from './heartbeat.controller';
 
 describe('HeartbeatService', () => {
   let service: HeartbeatService;
 
+  const mockHeartbeatModel = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [HeartbeatController],
       providers: [HeartbeatService],
-    }).compile();
+    })
+      .overrideProvider(HeartbeatService)
+      .useValue(mockHeartbeatModel)
+      .compile();
 
     service = module.get<HeartbeatService>(HeartbeatService);
   });
